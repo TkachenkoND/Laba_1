8ab24c225ccea1d4d4bb3194c3994e198f904a80
Пользователь <tkachenkond00@gmail.com>
2022-12-06 19:45:59 +0200

    final commit

--- Laba_1.cpp
+++ Laba_1.cpp
@@ -1,20 +1,112 @@
-// Laba_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+using namespace std;
+
+string add(string lhs, string rhs) {
+    int length = max(lhs.size(), rhs.size());
+    int carry = 0;
+    int sum_col;  
+    string result;
+
+    while (lhs.size() < length)
+        lhs.insert(0, "0");
+
+    while (rhs.size() < length)
+        rhs.insert(0, "0");
+
+    for (int i = length - 1; i >= 0; i--) {
+        sum_col = (lhs[i] - '0') + (rhs[i] - '0') + carry;
+        carry = sum_col / 10;
+        result.insert(0, to_string(sum_col % 10));
+    }
+
+    if (carry)
+        result.insert(0, to_string(carry));
+
+    return result.erase(0, min(result.find_first_not_of('0'), result.size() - 1));
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+string subtract(string lhs, string rhs) {
+    int length = max(lhs.size(), rhs.size());
+    int diff;
+    string result;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    while (lhs.size() < length)
+        lhs.insert(0, "0");
+
+    while (rhs.size() < length)
+        rhs.insert(0, "0");
+
+    for (int i = length - 1; i >= 0; i--) {
+        diff = (lhs[i] - '0') - (rhs[i] - '0');
+        if (diff >= 0)
+            result.insert(0, to_string(diff));
+        else {
+
+            int j = i - 1;
+            while (j >= 0) {
+                lhs[j] = ((lhs[j] - '0') - 1) % 10 + '0';
+                if (lhs[j] != '9')
+                    break;
+                else
+                    j--;
+            }
+            result.insert(0, to_string(diff + 10));
+        }
+
+    }
+
+    return result.erase(0, min(result.find_first_not_of('0'), result.size() - 1));
+}
+
+string multiply(string lhs, string rhs) {
+    int length = max(lhs.size(), rhs.size());
+
+    while (lhs.size() < length)
+        lhs.insert(0, "0");
+
+    while (rhs.size() < length)
+        rhs.insert(0, "0");
+
+    if (length == 1)
+        return to_string((lhs[0] - '0') * (rhs[0] - '0'));
+
+    string lhs0 = lhs.substr(0, length / 2);
+    string lhs1 = lhs.substr(length / 2, length - length / 2);
+    string rhs0 = rhs.substr(0, length / 2);
+    string rhs1 = rhs.substr(length / 2, length - length / 2);
+
+    string p0 = multiply(lhs0, rhs0);
+    string p1 = multiply(lhs1, rhs1);
+    string p2 = multiply(add(lhs0, lhs1), add(rhs0, rhs1));
+    string p3 = subtract(p2, add(p0, p1));
+
+    for (int i = 0; i < 2 * (length - length / 2); i++)
+        p0.append("0");
+    for (int i = 0; i < length - length / 2; i++)
+        p3.append("0");
+
+    string result = add(add(p0, p1), p3);
+
+    return result.erase(0, min(result.find_first_not_of('0'), result.size() - 1));
+}
+
+int main() {
+    string s1, s2;
+
+    cout << "Enter number 1 = ";
+    cin >> s1;
+    
+    cout << "Enter number 2 = ";
+    cin >> s2;
+
+    cout << "Result : " << multiply(s1, s2) << endl;
+    return 0;
+}
+
+
+
